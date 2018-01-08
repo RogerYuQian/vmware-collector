50049153d4901d7641ebc386baae276d9037df4f
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2018-01-08 16:04:33 +0800

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 # https://github.com/vmware/pyvmomi-community-samples/blob/836eddf1586a917710d4dcf2ab631fb6c1e45699/samples/esxi_perf_sample.py
 
 from prettytable import PrettyTable
+from multiprocessing.pool import ThreadPool
 
 from pyVmomi import vim
 from pyVim.connect import SmartConnectNoSSL, Disconnect
@@ -20,8 +21,9 @@ import datetime
 LOG = logging.getLogger(__name__)
 
 NEEDED_COUNTER = (
-    'cpu:usage:averagej',
-    'mem:consumed:average',
+    'cpu:usage:average',
+    # 'mem:consumed:average',
+    'mem:usage:average',
     # 'net:received:average',
     # 'net:transmitted:average',
     # 'disk:read:average',
@@ -41,14 +43,14 @@ NODE2 = {
     'host': '192.168.22.171',
     'user': 'administrator@vsphere.local',
     'pwd': '99Cloud!@#',
-    'port': 8443
+    'port': 443
 }
 
 
 class Vmware(object):
 
     def __init__(self):
-        si = SmartConnectNoSSL(**NODE1)
+        si = SmartConnectNoSSL(**NODE2)
         self.content = si.RetrieveContent()
         self.perfManager = self.content.perfManager
 
@@ -86,14 +88,21 @@ class Vmware(object):
         children = container_view.view
         for child in children:
             vm_power_state = child.summary.runtime.powerState
-            LOG.info('%s power state is: %s',  child, vm_power_state)
             if (power_state and
                     vm_power_state != power_state):
-                LOG.info('Skip %s state node: %s', power_state, child)
+                LOG.debug('Skip %s state node: %s', vm_power_state, child)
                 continue
             yield child
 
     def get_counters(self, nodes):
+        '''
+        {
+            "node_name": {
+                "key1": "value1",
+                "key2": "value2"
+            },
+        }
+        '''
         query_specs = []
         for node in nodes:
             needed_couter_ids = [_id for _id, name in self.counter_info.items()
@@ -104,7 +113,15 @@ class Vmware(object):
                                                           metricId=metric_ids,
                                                           entity=node)
             query_specs.append(query_spec)
-        return self.perfManager.QueryPerf(querySpec=query_specs)
+        results = self.perfManager.QueryPerf(querySpec=query_specs)
+        ret = {}
+        for node, items in zip(nodes, results):
+            node_uuid = node.summary.config.instanceUuid
+            ret[node_uuid] = {}
+            for item_name, item in zip(NEEDED_COUNTER, items.value):
+                ret[node_uuid][item_name] = item.value[0]
+        LOG.debug('Get: %s', ret)
+        return ret
 
 
 def print_result(nodes, results):
@@ -113,13 +130,20 @@ def print_result(nodes, results):
 
     x = PrettyTable(header)
     x.align['Node'] = 'r'
-    for node, result in zip(nodes, results):
-        data = [node.summary.config.name]
-        for v in result.value:
-            data.append(v.value[0])
-        if len(data) < len(header):
-            data.extend(['-'] * (len(header) - len(data)))
-        x.add_row(data)
+
+    try:
+        for node in nodes:
+            node_uuid = node.summary.config.instanceUuid
+            data = [node.summary.config.name]
+            items = results.get(node_uuid)
+            if not items:
+                LOG.warning('Unable get instance counters for %s', node)
+                continue
+            for counter in NEEDED_COUNTER:
+                data.append(items.get(counter, '-'))
+            x.add_row(data)
+    except Exception:
+        LOG.exception('')
     print(x)
 
 
@@ -139,29 +163,99 @@ def timing(times=1):
 
 def performance():
 
-    @timing(times=10)
+    # Get basic information
+
+    def get_info():
+
+        vmware = Vmware()
+        nodes = list(vmware.get_all_nodes())
+        up_nodes = list(vmware.get_all_power_on_nodes())
+        LOG.info('Get %d nodes: %s', len(nodes), nodes)
+        LOG.info('Get %d up nodes: %s', len(up_nodes), up_nodes)
+
+    @timing(times=1)
+    def get_vmware_and_nodes():
+        vmware = Vmware()
+        # nodes = vmware.get_all_power_on_nodes()
+        nodes = vmware.get_all_nodes()
+        return vmware, nodes
+
+    @timing(times=1)
     def one_by_one():
-        vmware = Vmware()
-        nodes = vmware.get_all_nodes()
-        for node in nodes:
-            vmware.get_counters([node])
+        vmware, nodes = get_vmware_and_nodes()
 
-    @timing(times=10)
+        @timing(times=1)
+        def inner():
+            for node in nodes:
+                vmware.get_counters([node])
+        inner()
+
+    @timing(times=1)
     def many():
-        vmware = Vmware()
-        nodes = vmware.get_all_nodes()
-        vmware.get_counters(nodes)
+        vmware, nodes = get_vmware_and_nodes()
 
+        @timing(times=1)
+        def inner():
+            vmware.get_counters(nodes)
+        inner()
+
+    @timing(times=1)
+    def run_concurrency():
+        vmware, nodes = get_vmware_and_nodes()
+        pool = ThreadPool(3)
+
+        @timing(times=1)
+        def inner():
+            returns = []
+            for sub_nodes in group(nodes, 4):
+                returns.append(pool.apply_async(vmware.get_counters,
+                                                (sub_nodes,)))
+            for ret in returns:
+                ret.get(0xff)
+                # results.update(ret.get(0xff))
+        inner()
+
+    get_info()
+
+    run_concurrency()
     one_by_one()
     many()
 
 
 def get_one_by_one():
     vmware = Vmware()
-    nodes = vmware.get_all_nodes()
+    nodes = vmware.get_all_power_on_nodes()
     for node in nodes:
         results = vmware.get_counters([node])
         print_result([node], results)
+
+
+def group(eles, count):
+    itr = iter(eles)
+    while True:
+        ret = []
+        try:
+            for i in range(count):
+                ret.append(itr.next())
+            yield ret
+        except StopIteration:
+            if ret:
+                yield ret
+            raise StopIteration
+
+
+def run_multi_process():
+    pool = ThreadPool(10)
+    vmware = Vmware()
+    nodes = vmware.get_all_power_on_nodes()
+    returns = []
+
+    for sub_nodes in group(nodes, 1):
+        returns.append(pool.apply_async(vmware.get_counters, (sub_nodes,)))
+    results = {}
+    for ret in returns:
+        results.update(ret.get(0xff))
+    print_result(nodes, results)
 
 
 def main():
@@ -172,5 +266,12 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    globals()[sys.argv[1]]()
+    if len(sys.argv) < 2:
+        func = "performance"
+    else:
+        func = sys.argv[1]
+    if func == 'performance':
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+    globals()[func]()
